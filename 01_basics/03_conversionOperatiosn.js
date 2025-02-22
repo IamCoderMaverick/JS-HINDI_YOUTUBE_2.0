f0b1fc758ab9a70b32b772b48bc66658edd9a45a
IamCoderMaverick <iamcoderMaverick@gmail.com>
2025-02-22 11:54:05 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperatiosn.js
@@ -0,0 +1,48 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+// let score = "Hitesh"
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+
+// let valueInNummber = Number(score)
+// console.log(typeof valueInNummber);
+// console.log(valueInNummber)
+
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false =>0 
+
+// // let isLoggedIN = 1 // true
+// let isLoggedIN =  ""
+// // let isLoggedIN = 0 // false
+
+
+
+// let BooleanIsLoggedIn = Boolean(isLoggedIN)
+
+// console.log(BooleanIsLoggedIn);
+// 1 => true ; 0 => false
+// "" => false
+//  "hitesh" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
+
