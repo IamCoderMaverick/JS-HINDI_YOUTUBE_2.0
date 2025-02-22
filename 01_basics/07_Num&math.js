14fd795a00cd7271b68a699a9ea6ed2045749a8c
IamCoderMaverick <iamcoderMaverick@gmail.com>
2025-02-22 16:36:31 +0000

    num&math

---
+++ 01_basics/07_Num&math.js
@@ -0,0 +1,51 @@
+const score = 400
+
+const balance = new Number(100) //defining number explicitly
+
+// console.log(score);
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); 
+
+
+const otherNumber = 123.8966
+// const otherNumber = 23.8966
+
+// console.log(otherNumber.toPrecision(4)); //precise value will round of if decimal is there
+
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN')); //for indian standard 
+
+
+// **************************** MATHS ******************************
+
+// console.log(Math);
+// console.log(Math.abs(-4)); //absolute value negative to positive
+// console.log(Math.round(4.6)); //round off
+// console.log(Math.ceil(4.2)); //top value
+// console.log((Math.floor(4.9))); //bottom value
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+
+// console.log(Math.random()); //always value b/w 0 and 1
+// console.log(Math.random()*10+1); 
+// console.log((Math.random()*10)+1); 
+console.log(Math.floor(Math.random()*10)+1); 
+
+const min = 10 
+const max = 20
+
+console.log(Math.floor((Math.random() * (max - min + 1)) + min))
+
+
+
+
+
+
+
+
+
