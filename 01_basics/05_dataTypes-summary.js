af4f7397fe5cb0aed2881fa9a67f855759a5976d
IamCoderMaverick <iamcoderMaverick@gmail.com>
2025-02-22 12:39:40 +0000

    datatype summary

---
+++ 01_basics/05_dataTypes-summary.js
@@ -0,0 +1,44 @@
+// Primitive - can't allocate to memory
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; //undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 345653457364763343n
+
+
+// Refrence (Non primitive) - can allocate refrence in Memory
+
+// Array, Objects, Functions //data types always functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+
+const myFunction = function(){
+    console.log("hello World");
+    
+}
+
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
+console.log(typeof myFunction); //function object
+
+
+console.log(typeof id);
+
+
