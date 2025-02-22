e6e557befe459193aa92df3fd2c898d9dc6d730d
IamCoderMaverick <iamcoderMaverick@gmail.com>
2025-02-22 12:21:33 +0000

    Comparisons

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,47 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined <= 0);
+// console.log(undefined >= 0);
+
+// avoid above comparisons
+
+// The reasonn is that an equality check == and comparison > < >= <= works differently.
+// Comparisons convert null to a number, treating it as 0.
+// Thats why(14) num >= 0 is true and (1) null >0 is false.
+
+
+
+
+
+// ===  it checks everything datatypes,sting,numbers
+
+console.log("2" === 2);
+
+
+
+
+
+
+
+
+
+
+
+
+
